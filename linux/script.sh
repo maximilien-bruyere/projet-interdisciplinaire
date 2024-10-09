200a81de99e96eb77287b69bfe040fa6f02756c6
Maximilien Bruyère <maximilien-bruyere@hotmail.com>
2024-10-09 09:50:23 +0200

    [docs](linux) update script.sh - add description

---
+++ linux/script.sh
@@ -0,0 +1,32 @@
+#!/bin/bash 
+
+# ID : 10092024
+# Author : Maximilien Bruyère
+# OS : Fedora 40 - Server Edition
+
+###############
+# DESCRIPTION #
+###############
+#
+# This script is used to install a LAMP stack on a Fedora 40 Server Edition.
+#
+# LAMP : 
+#   L : Linux
+#   A : Apache
+#   M : MariaDB
+#   P : PHP
+#
+# It will work with an AD and DNS server (Windows Server 2019) and 
+# a client (Windows 10).
+#
+###############
+
+############
+# WARNINGS #
+############
+#
+# Must be run as root.
+# Must be run on a Fedora 40 Server Edition.
+# Must be run after the installation of the AD and DNS server (Windows Server 2019).
+#
+############
