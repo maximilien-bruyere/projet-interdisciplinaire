d9c9b48ee1e30ec14a8680988b1cde6eb1875ecf
Maximilien Bruyère <maximilien-bruyere@hotmail.com>
2024-10-14 14:47:43 +0200

    [style](linux) update script.sh file

--- linux/script.sh
+++ linux/script.sh
@@ -49,14 +49,19 @@ check_root() {
 }
 
 update_system() {
+    clear
     echo -e "\n${BLUE}------------${ENDCOLOR}"
     echo -e "${BLUE}   Update   ${ENDCOLOR}"
     echo -e "${BLUE}------------${ENDCOLOR}\n"
+
     dnf update -y 
     dnf upgrade -y 
+
+    echo -e "\n${GREEN}Update done.${ENDCOLOR}\n"
 }
 
 delete_users() {
+    clear
     echo -e "\n${BLUE}----------------${ENDCOLOR}"
     echo -e "${BLUE}   Delete User  ${ENDCOLOR}"
     echo -e "${BLUE}----------------${ENDCOLOR}\n"
@@ -64,29 +69,35 @@ delete_users() {
     read -p "Enter the UID of the user to delete : " UID
     find / -uid $UID -exec rm -rf {} \;
     userdel -r $UID
+
+    echo -e "\n${GREEN}User deleted.${ENDCOLOR}\n"
+    clear
 }
 
 global_configuration() {
+    clear
     echo -e "\n${BLUE}--------------------------${ENDCOLOR}"
     echo -e "${BLUE}   Global Configurations  ${ENDCOLOR}"
     echo -e "${BLUE}--------------------------${ENDCOLOR}\n"
 
-    read -p "Enter the hostname : " HOSTNAME
-    read -p "Enter the domain (ex : .[com, be, lan]) : " DOMAIN
+    read -p "Enter the hostname (ex : [fedora].WindowsServer2019.lan) : " HOSTNAME
     read -p "Enter the server name (ex : [WindowsServer2019].lan) : " SERVERNAME
-
+    read -p "Enter the domain (ex : .[lan]) : " DOMAIN
     echo "HOSTNAME=$HOSTNAME" >> ./config.conf
     echo "DOMAIN=$DOMAIN" >> ./config.conf
     echo "SERVERNAME=$SERVERNAME" >> ./config.conf
-
     hostnamectl set-hostname $HOSTNAME
     rm -rf /etc/resolv.conf
+    systemctl restart NetworkManager
 
+    echo -e "\n${GREEN}Hostname changed.${ENDCOLOR}\n"
+
+    # All users will got as name : [familyname].[firstname]
+    users_list=()
     read -p "How many people need access to the server ? : [number] " NUMBERPEOPLE
-    for (( user=1; user<=$NUMBERPEOPLE; user++))
-    do 
+    for (( user=1; user<=$NUMBERPEOPLE; user++)); do 
         echo -e "\nUser number : $user"
-        echo -e " ----------------\n"
+        echo -e "----------------\n"
         read -p "What's the username number ? : [string] " USERNAME
 
         if [ $(id -u $USERNAME) ]
@@ -94,6 +105,8 @@ global_configuration() {
             echo -e "${RED}This username already exists.${ENDCOLOR}"
             sudo -u $USERNAME ssh-keygen -t rsa -b 4096 -C "$USERNAME@$HOSTNAME.$SERVERNAME.$DOMAIN"
             mv /home/$USERNAME/.ssh/id_rsa.pub /home/$USERNAME/.ssh/authorized_keys
+
+            users_list+=($USERNAME)
 
             continue
         fi
@@ -106,6 +119,8 @@ global_configuration() {
             passwd $USERNAME
             usermod -aG wheel $USERNAME
 
+            users_list+=($USERNAME)
+
             sudo -u $USERNAME ssh-keygen -t rsa -b 4096 -C "$USERNAME@$HOSTNAME.$SERVERNAME.$DOMAIN"
             mv /home/$USERNAME/.ssh/id_rsa.pub /home/$USERNAME/.ssh/authorized_keys
 
@@ -113,13 +128,18 @@ global_configuration() {
             useradd -m $USERNAME
             passwd $USERNAME
 
+            users_list+=($USERNAME)
+
             sudo -u $USERNAME ssh-keygen -t rsa -b 4096 -C "$USERNAME@$HOSTNAME.$SERVERNAME.$DOMAIN"
             mv /home/$USERNAME/.ssh/id_rsa.pub /home/$USERNAME/.ssh/authorized_keys
         fi 
     done
 
-    # scp avec port 
-
+    echo -e "\n${GREEN}Users and ssh key created.${ENDCOLOR}\n"
+    
+    echo -e "-----------------------------------"
+    echo -e "${RED}scp [user]@[server-ip]:/home/[user]/.ssh/id_rsa C:/users/[windows-user]/.ssh/${ENDCOLOR}"
+    echo -e "${RED}Please, transfer the private keys to : ${ENDCOLOR}\n" 
 
     # After the loop, please transfer the private keys generated
     # to the users who need them. 
@@ -129,16 +149,18 @@ global_configuration() {
     # In this project, I advise you to use Termius / Putty to connect yourself to the server.
     # To have access to the server, you MUST use PuttyGen (load the private key) and
     # save the private key in the .ppk format.
-
-    echo -e "\n${RED}Please, transfer the private keys to the users who need them.${ENDCOLOR}"
-    echo -e "${RED}To transfer them, use the scp command :${ENDCOLOR}"
-    echo -e "${RED}scp [user]@[server-ip]:/home/[user]/.ssh/id_rsa C:/users/[windows-user]/.ssh/${ENDCOLOR}\n"
-
     # -n1 : one character
     # -s : silent
-    read -p "Press any key to continue..." -n1 -s 
-    systemctl restart NetworkManager
-    clear
+
+    for user in ${users_list[@]}; do
+        echo -e "- $user" 
+    done 
+
+    echo -e ""
+    read -p "Press any key to continue... " -n1 -s 
+    echo -e "\n-----------------------------------"
+
+    echo -e "\n${GREEN}Configuration done.${ENDCOLOR}\n"
 }
 
 ssh_configuration() {
@@ -146,20 +168,23 @@ ssh_configuration() {
     echo -e "${BLUE}  SSH Configuration  ${ENDCOLOR}"
     echo -e "${BLUE}---------------------${ENDCOLOR}\n"
 
-    read -p "Choose port for ssh : [number]" PORT
+    read -p "Choose port for ssh : " PORT
     sed -i "21s/#//; 21s/22/$PORT/" /etc/ssh/sshd_config
     sed -i "40s/#//; 40s/prohibit-password/no/" /etc/ssh/sshd_config
     sed -i "65s/#//; 65s/yes/no/" /etc/ssh/sshd_config
     sed -i "66s/#//" /etc/ssh/sshd_config
     sed -i "69s/#//; 65s/yes/no/" /etc/ssh/sshd_config
     sed -i "96s/#//; 96s/no/yes/" /etc/ssh/sshd_config
-    semanage port -a -t ssh_port_t -p tcp $PORT
+
+    semanage port -a -t ssh_port_t -p tcp $PORT 
 
     firewall-cmd --add-port=$PORT/tcp --permanent
     firewall-cmd --remove-service=ssh --permanent
 
     systemctl restart firewalld
     systemctl restart sshd
+
+    echo -e "\n${GREEN}SSH and firewalld configurations done.${ENDCOLOR}\n"
     clear
 }
 
@@ -172,6 +197,8 @@ add_services() {
     apache_configuration
     database_configuration
     php_configuration
+
+    echo -e "\n${GREEN}Services configurations done.${ENDCOLOR}\n"
 }
 
 backup_configuration() {
